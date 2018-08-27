de08023ca1f5e4e372b640855e108bf095c5263b
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-08-26 21:42:43 -0500

    Add files via upload

---
+++ Java/JavaTM.java
@@ -0,0 +1,43 @@
+/*
+Kevin Nguyen
+Basic Java program to transpose a given matrix.
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaShell"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class JavaShell {
+	//Global Declarations
+
+	//main driver
+	public static void main(String[] args) {
+		//local Declarations
+		System.out.println("HelloWorld");
+	}
+
+	//function definitions below
+    public int[][] transpose(int[][] A) {
+        //local declarations
+        int i = 0, j = 0, colLen = A[0].length, rowLen = A.length;
+        int[][] result = new int[colLen][rowLen]; //new matrix with sizes flipped for transposition
+        
+        //outer loop iterate by columns
+        while(j < colLen) {
+            //inner loop iterate by rows
+            while(i < rowLen) {
+                result[j][i] = A[i][j]; //fill resulting matrix
+                i++;
+            }
+            j++; i=0;
+        }
+        
+        return result;
+    }	
+}
